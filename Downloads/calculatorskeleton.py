aef37e5bbd2d5bb8f4ab0f4000cb84d060c5672c
Andy <Ultramaster123456@gmail.com>
2026-02-16 18:01:29 -0800

    testpush

---
+++ Downloads/calculatorskeleton.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import tkinter.messagebox
+from tkinter.constants import SUNKEN
+
+win = tk.Tk()
+win.title('')
+
+frame = tk.Frame()
+
+entry = tk.Entry()
+
+def click(num):
+    
+def equal():
+
+def clear():
+
+buttons = []
+
+win.mainloop()
